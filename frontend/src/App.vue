f651a19bfa1d1d854bd72413ef08c37ff0281f35
Airton Silveira <airton.developer@gmail.com>
2020-09-21 11:40:01 -0300

    enviando o projeto ao repo

---
+++ frontend/src/App.vue
@@ -0,0 +1,36 @@
+<template> <!--enviando o projeto-->
+
+  <div id="app">   
+	<section>
+		<b-nav class="top">
+    <router-link to="/"><button class="btn btn-secondary">Home</button></router-link>
+    <router-link to="/cadastro"><button class="btn btn-secondary">Cadastros</button></router-link>
+  </b-nav>
+	</section>
+  
+	<router-view />   
+  </div>
+</template>
+<script>
+  export default {
+    data() { 
+      return {
+      }
+    }
+  };
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+.top{
+  top: 10;
+  background-color: #2c3e50;
+}
+
+
+</style>
---
+++ frontend/src/views/Cadastro.vue
@@ -0,0 +1,42 @@
+<template>
+	<div id="app" class="container">
+		<h1>MEDPREV</h1>
+        <b-card no-body>
+    <b-tabs card>
+      <b-tab title="CADASTRO CPF" active>
+        <b-card-text><CadastroCpf/></b-card-text>
+      </b-tab>
+      <b-tab title="CADASTRO CNPJ">
+        <b-card-text><CadastroCnpj/></b-card-text>
+      </b-tab>
+    </b-tabs>
+  </b-card>
+		
+	</div>
+</template>
+
+<script>
+import CadastroCpf from "@/components/CadastroCpf.vue";
+import CadastroCnpj from "@/components/CadastroCnpj.vue";
+export default {
+    components: { 
+        CadastroCpf,
+        CadastroCnpj
+    },
+}
+</script>
+
+<style>
+#app {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	color: #2c3e50;
+	font-size: 1.5rem;
+}
+
+#app h1 {
+	text-align: center;
+	margin: 50px;
+}
+</style>
